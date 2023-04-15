0ae8718b0efae5df94beba7fd6f052e42130abda
akshay162 <akshay.gpt1602@gmail.com>
2023-04-15 14:01:19 +0530

    added filePath function in main

--- src/main/java/com/navi/ledger/Main.java
+++ src/main/java/com/navi/ledger/Main.java
@@ -4,6 +4,7 @@ import com.navi.ledger.processor.CommandProcessor;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -12,12 +13,8 @@ public class Main {
         CommandProcessor commandProcessor = new CommandProcessor();
 
         try {
-            // the file to be opened for reading
-//            FileInputStream fis = new FileInputStream(args[0]);
 
-            String testInputFilePath = "sample_input/input1.txt";
-            String absolutePath = new File(testInputFilePath).getAbsolutePath();
-            FileInputStream fis = new FileInputStream(absolutePath);
+            FileInputStream fis = new FileInputStream(getAbsoluteFilePath(args));
             Scanner sc = new Scanner(fis);
             while (sc.hasNextLine()) {
                 String commandLine = sc.nextLine();
@@ -27,5 +24,18 @@ public class Main {
         } catch (IOException e) {
             System.out.println("Error while processing commands from file");
         }
+    }
+
+    public static String getAbsoluteFilePath(String[] args) throws FileNotFoundException {
+
+//        The file to be opened for reading,
+//        if run through cli, pass the file path as argument and uncomment the below line
+//        FileInputStream fis = new FileInputStream(args[0]);
+
+        // uncomment the below two lines if file is being passed as an argument while running through cli
+        String testInputFilePath = "sample_input/input1.txt";
+        String absolutePath = new File(testInputFilePath).getAbsolutePath();
+
+        return absolutePath;
     }
 }
