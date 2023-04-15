9c4aab26c812cfa5b0220187266b9737085b4afa
akshay162 <akshay.gpt1602@gmail.com>
2023-04-15 13:49:09 +0530

    first commit

--- run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-mvn clean install -DskipTests assembly:single -q
-java -jar target\naviledger.jar sample_input\input1.txt
--- src/main/java/com/navi/ledger/utils/exceptions/ElementNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.navi.ledger.utils.exceptions;
-
-public class ElementNotFoundException extends RuntimeException {
-
-    public ElementNotFoundException(String message) {
-        super(message);
-    }
-}
